e148b17f49077b620ae6c2debe7eac1fbc9780f1
may <maymay1@gmail.com>
2024-12-13 15:20:08 +0200

    Updated Hello.java to include a new print message

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello{
      { 
         for (int i=1;i<=10; i++) 
         {
-            System.out.println("hello world"+i); 
+            System.out.println("hello java hello java"+i); 
         }
      }
 
